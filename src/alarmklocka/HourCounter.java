46c30f9b26ddf118804605d7f12be861495c008a
moham <moham@130.243.2.64>
2023-08-30 17:56:57 +0200

    skapat classen hourcounter

---
+++ src/alarmklocka/HourCounter.java
@@ -0,0 +1,23 @@
+package alarmklocka;
+
+public class HourCounter extends CircularCounter {
+
+		public HourCounter() {
+			this(Direction.INCREASING, null); // vad anropas här?
+		}
+
+		public HourCounter(CounterType next) {
+			this(Direction.INCREASING, next); // vad anropas här?
+		}
+
+		public HourCounter(Direction direction) {
+			this(direction, null); // vad anropas här?
+		}
+
+		public HourCounter(Direction direction, CounterType next)
+		 {
+		 super(2, direction, next); // vad anropas här?
+	
+
+}
+}
