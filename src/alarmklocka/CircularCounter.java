3144cbb7e63cd3358dadd25ecd5f0d8ba65f1533
22joka04 <22joka04@HW-RDSSH-03.HIG-AD.hig.se>
2023-08-31 10:59:27 +0200

    fixed problem

--- src/alarmklocka/CircularCounter.java
+++ src/alarmklocka/CircularCounter.java
@@ -27,52 +27,53 @@ public abstract class CircularCounter implements CounterType {
 	}
 
 	@Override
-	public int getCount() { return currentCount; }
+	public int getCount() {
+		return currentCount;
+	}
 
 	// Starta om räknare från början
 	@Override
 	public void reset() {
 		currentCount = 0;
 	}
+
 	@Override
 	public void pause() {
 		isPaused = true;
 	}
+
 	@Override
-	public void resume(){ 
+	public void resume() {
 		isPaused = false;
 	}
 
 	@Override
-	public void count()
-	{
-	if (!isPaused && this.MAX_NR_OF_COUNTS>0)
-	{
-	if (direction == Direction.INCREASING)
-	{
-	currentCount ++;
-	//Kolla om räknaren har räknat ett helt varv
-	if (currentCount >= MAX_NR_OF_COUNTS)
-	{
-	reset(); //Se till att räknaren börjar om från noll
-	//Om kopplad till en annan räknare (nextCounter)...
-	//if (nextCounter != null)
-	//??? //...räkna upp den andra räknaren ett steg.
-	}
-	}
-	else if (direction == Direction.DECREASING)
-	{
-	if(currentCount > 0) {
-		currentCount--;//Flera kod-rader saknas här
-	}
-	}
-	}
+	public void count() {
+		if (!isPaused && this.MAX_NR_OF_COUNTS > 0) {
+			if (direction == Direction.INCREASING) {
+				currentCount++;
+				// Kolla om räknaren har räknat ett helt varv
+				if (currentCount >= MAX_NR_OF_COUNTS) {
+					if (nextCounter != null) {
+						reset();
+						nextCounter.count();
+					}
+					// Se till att räknaren börjar om från noll
+					// Om kopplad till en annan räknare (nextCounter)...
+					// if (nextCounter != null)
+					// ??? //...räkna upp den andra räknaren ett steg.
+				}
+			} else if (direction == Direction.DECREASING) {
+				if (currentCount > 0) {
+					currentCount--;// Flera kod-rader saknas här
+				}
+			}
+		}
 	}
 
 	@Override
-	public String toString()
-	{
-	return "" + currentCount;
+	public String toString() {
+		return "" + currentCount;
 	}
 
 }
--- src/alarmklocka/TestMinute.java
+++ src/alarmklocka/TestMinute.java
@@ -12,11 +12,13 @@ class TestMinute {
 	void test() {
 		HourCounter hourC = new HourCounter();
 		MinuteCounter myCounter = new MinuteCounter(Direction.INCREASING, hourC);
-		 assertEquals(0,myCounter.getCount());
-		 myCounter.count();
-		 assertEquals(1,myCounter.getCount());
-		 myCounter.count();
-		 assertEquals(2,myCounter.getCount());
+		SecondCounter secondC = new SecondCounter(Direction.INCREASING, myCounter);
+		
+		for(int i = 0; i < 1500; i++ ) {
+			myCounter.count();
+		}
+
+		 System.out.println("It has passed: " + hourC.getCount() + ":" + myCounter.getCount() + ":" + secondC.getCount());
 	}
 	
 	
