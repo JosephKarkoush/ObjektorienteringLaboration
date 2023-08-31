ce1532f6687b771395768100d9093953b5bca3d8
moham <moham@130.243.2.64>
2023-08-31 10:17:47 +0200

    joseph sa comita

--- src/alarmklocka/TestMinute.java
+++ src/alarmklocka/TestMinute.java
@@ -4,16 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import alarmklocka.CircularCounter.Direction;
+
 class TestMinute {
 
 	@Test
 	void test() {
-		CounterType myCounter = new MinuteCounter();
+		HourCounter hourC = new HourCounter();
+		MinuteCounter myCounter = new MinuteCounter(Direction.INCREASING, hourC);
 		 assertEquals(0,myCounter.getCount());
 		 myCounter.count();
 		 assertEquals(1,myCounter.getCount());
 		 myCounter.count();
-		 assertEquals(0,myCounter.getCount());
+		 assertEquals(2,myCounter.getCount());
 	}
 	
 	
