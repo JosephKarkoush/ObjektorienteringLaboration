977c17dea422297df3c3380851ef3ba475289d49
moham <moham@130.243.2.64>
2023-08-31 09:39:25 +0200

    skapat junit

---
+++ src/alarmklocka/TestCounter.java
@@ -0,0 +1,14 @@
+package alarmklocka;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCounter {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
